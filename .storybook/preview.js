bb464111c04a5c341f4786bdad2a9d8362326acf
autumn crossan forsythe <the@autumn.zone>
2022-01-26 13:17:47 -0500

    initial commit

---
+++ .storybook/preview.js
@@ -0,0 +1,12 @@
+const xstate = require("xstate");
+window.x = xstate;
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
---
+++ stories/Stoplight.stories.js
@@ -0,0 +1,9 @@
+import stoplight from "./stoplight.html";
+import edit from "./edit.html";
+
+export default {
+  title: "xstate",
+};
+
+export const Stoplight = () => stoplight;
+export const EditMode = () => edit;
